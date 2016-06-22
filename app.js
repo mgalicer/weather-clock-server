95285bda1779429b28df6f689998deb1c902c20a
Mari <marigalicer@gmail.com>
2016-06-22 13:21:20 -0400

    switch to getJSON module

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var app = express();
-var request = require("request");
+'use strict';
 
-var options = { method: 'GET',
-  url: 'https://api.forecast.io/forecast/' + process.env.WEATHER_KEY + '/40.6974881,-73.979681',
-  qs: { exclude: '[minutely,alerts,flags,daily]' },
-};
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const getJSON = require('get-json');
+const weatherURL = 'https://api.forecast.io/forecast/' + process.env.WEATHER_KEY + '/40.6974881,-73.979681?exclude=[minutely,alerts,flags,daily]';
 
-app.get('/weather', function (req, res) {
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
 
-    res.send(body);
-  });
+getJSON(weatherURL, function(err, response) {
+  console.log(response)
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.get('/weather', (req, res) => {
+//   res.render(weatherData)
+// });
+
+// app.listen(3000, () => {
+//   console.log('Example app listening on port 3000!');
+// });
 
 
