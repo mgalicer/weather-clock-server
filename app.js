efeb0bf32b69697f64d7850e804c92c8602c6cb2
Mari <marigalicer@gmail.com>
2016-06-22 17:34:16 -0400

    save weather data in json

--- app.js
+++ app.js
@@ -6,17 +6,21 @@ const app = express();
 const getJSON = require('get-json');
 const weatherURL = 'https://api.forecast.io/forecast/' + process.env.WEATHER_KEY + '/40.6974881,-73.979681?exclude=[minutely,alerts,flags,daily]';
 
-
-getJSON(weatherURL, function(err, response) {
-  console.log(response)
+app.get('/weather', (req, res) => {
+  getJSON(weatherURL, (err, response) => {
+    let hourlyWeather = [];
+    const weatherData = response.hourly.data;
+    for(var i = 0; i < 12; i++) {
+      let obj = {};
+      obj.time = weatherData[i].time;
+      obj.temp = weatherData[i].apparentTemperature;
+      obj.precip = weatherData[i].precipProbability;
+      hourlyWeather.push(obj);
+    }
+    res.send(hourlyWeather)
+  })
 });
 
-// app.get('/weather', (req, res) => {
-//   res.render(weatherData)
-// });
-
-// app.listen(3000, () => {
-//   console.log('Example app listening on port 3000!');
-// });
-
-
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
